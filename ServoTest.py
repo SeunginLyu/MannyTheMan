92ad74a484e5f0fbd13a9b8733b5492965a550ff
Allison Basore <allison.basore@students.olin.edu>
2017-10-21 21:17:06 +0000

    added three more servos

--- ServoTest.py
+++ ServoTest.py
@@ -1,28 +1,38 @@
-
-
 import RPi.GPIO as GPIO
+
 from time import sleep
 
-x = 3
+outPinA = 2
+outPinB = 3
+outPinC = 4
+outPinD = 7
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(x, GPIO.OUT)
 
-pwm=GPIO.PWM(x, 50)
+GPIO.setup(outPinA, GPIO.OUT)
+GPIO.setup(outPinB, GPIO.OUT)
+GPIO.setup(outPinC, GPIO.OUT)
+GPIO.setup(outPinD, GPIO.OUT)
+
+pwm=GPIO.PWM(outPinD, 50)
+
 pwm.start(0)
 
 
-def SetAngle(angle):
-	duty = angle / 18 + 2
-	GPIO.output(x, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(x, False)
-	pwm.ChangeDutyCycle(0)
+def SetAngle(angle, pin):
+    duty = angle / 18 + 2
 
-SetAngle(90) 
+    GPIO.output(pin, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
 
+    GPIO.output(pin, False)
+    pwm.ChangeDutyCycle(0)
+
+SetAngle(90, outPinA)
+SetAngle(90, outPinB)
+SetAngle(90, outPinC)
+SetAngle(90, outPinD)
 
 pwm.stop()
 GPIO.cleanup()
-
