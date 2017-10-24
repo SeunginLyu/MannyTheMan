6ef9c5f1b87ddb5f7d26cdbe78c35e4d36c78368
AllisonBasore <allison.basore@students.olin.edu>
2017-10-24 18:53:00 -0400

    updated working servotest code

--- ServoTest.py
+++ ServoTest.py
@@ -1,41 +1,47 @@
 import RPi.GPIO as GPIO
 import models
-
-from time import sleep
+import time
 
 # RaspberryPi Pin initialization
 outPinA = 2
 outPinB = 3
 outPinC = 4
-outPinD = 7
+outPinD = 17
 
 GPIO.setmode(GPIO.BCM)
 
+# Initialies servo output pins
 GPIO.setup(outPinA, GPIO.OUT)
 GPIO.setup(outPinB, GPIO.OUT)
 GPIO.setup(outPinC, GPIO.OUT)
 GPIO.setup(outPinD, GPIO.OUT)
 
-pwm = GPIO.PWM(outPinD, 50)
-pwm.start(0)
+skeleton = models.Skeleton(models.sample_names,
+                           models.sample_links,
+                           models.sample_servos)
 
+skeleton.actuateMotion(models.sample_motions[2])
 
 
 def setServoAngle(angle, pin):
+    pwm = GPIO.PWM(pin, 50)
+    pwm.start(0)
     duty = angle / 18 + 2
-
     GPIO.output(pin, True)
     pwm.ChangeDutyCycle(duty)
-    sleep(1)
-
+    time.sleep(1)
     GPIO.output(pin, False)
     pwm.ChangeDutyCycle(0)
+    pwm.stop()
 
 
-setServoAngle(90, outPinA)
-setServoAngle(90, outPinB)
-setServoAngle(90, outPinC)
-setServoAngle(90, outPinD)
+refresh_period = 0.02
 
-pwm.stop()
+for joint in skeleton.joints:
+    setServoAngle(0, joint.servo)
+
+for joint in skeleton.joints:
+    setServoAngle(joint.angle, joint.servo)
+    print(joint.angle, joint.servo)
+
 GPIO.cleanup()
