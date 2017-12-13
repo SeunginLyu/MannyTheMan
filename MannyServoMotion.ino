42074e0deeff4bdda7814803b20bd5241f276a88
Allison Basore <allison.basore@students.olin.edu>
2017-12-13 15:19:43 -0500

    Updated with calibration values

--- MannyServoMotion.ino
+++ MannyServoMotion.ino
@@ -10,7 +10,8 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
 int currentpositions[] ={0,0,0,0,0,0,0,0,0}; //{90,90,90,90,90,90,90,90,90};
 int desiredpositions[] = {0,0,0,0,0,0,0,0,0};
-//1,90,2,90,3,90,4,90,5,90,6,90,7,90,8,90,
+// Example Input:
+// 1,90,2,90,3,90,4,90,5,90,6,90,7,90,8,90,
 int holderVec[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //the odd index's are the servos and the even index's are the angles that are going to be fed to the arduino
 int numberOfServos = 8 + 1;//allows if statement to filter through the different servos
 int timeRec = millis(); //returns the number of milliseconds the current sketch has been running since the last reset.
@@ -30,33 +31,13 @@ void setup() {
 
 //Finds the angles; senses the angle and converts it to the relative arduino angle
 //ex: if the angle is less than or equal to 10 than we know that the angle is around 120 because the range has a buffer around 20, we tested all the angles individually to ensure that these values match with the anglesint AngleFind(int ang){
-  if(ang <= 10){
-    return 120;
+
+int AngleFind(int ang){
+  int a = ((2.7*ang)+83);
+  if(a<140){
+    a = 140;
   }
-  if(ang <= 30){
-    return 220;
-  }
-  if(ang <= 50){
-    return 270;
-  }
-  if(ang <= 70){
-    return 320;
-  }
-  if(ang <= 90){
-    return 370;
-  }
-  if(ang <= 110){
-    return 420;
-  }
-   if(ang <= 130){
-    return 470;
-  }
-  if(ang <= 150){
-    return 520;
-  }
-  if(ang <= 180){
-    return 540;
-  }
+  return a;
 }
 
 
@@ -98,14 +79,70 @@ void loop() {
         byte index2 = holderString.indexOf(',');
         int AngleVal = holderString.substring(0, index2).toInt();
         holderString = holderString.substring(index2 + 1,holderString.length());
+         
+         // Setting the specific constraints on the joints.
+
+        if(ServoNum == 8){ 
+           // Bottom Right Leg
+          AngleVal = 90 - AngleVal;
+          if(AngleVal < 25){
+            AngleVal = 25;
+          }
+          if(AngleVal > 70){
+            AngleVal = 70;
+          }
+        }
+
+        if(ServoNum == 7){
+          // Bottom Left Leg
+          AngleVal = 90 - AngleVal;
+          if(AngleVal < 45){
+            AngleVal = 45;
+          }
+          if(AngleVal > 90){
+            AngleVal = 90;
+          }
+        }
+
+        if(ServoNum == 5){ 
+          // Top Right Leg
+            AngleVal = 85;
+        }
+         
+        if(ServoNum == 6){ 
+          // Top Left Leg
+            AngleVal = 10;
+        }
+         
+        if(ServoNum == 4){ 
+          // Right Shoulder
+          if(AngleVal > 170){
+            AngleVal = 170;
+          }
+           AngleVal = 180-AngleVal; 
+        }
+
+        if(ServoNum == 3){
+          // Left Shoulder
+          if(AngleVal > 150){
+            AngleVal = 150;
+          }
+        }
+         
+        if(ServoNum == 2){ 
+          // Right Arm
+            AngleVal = 180-AngleVal; 
+        }
+
+        if(ServoNum == 1){ 
+          // Left Arm
+            AngleVal = 180-AngleVal; 
+        }
+        
         desiredpositions[ServoNum] = AngleFind(AngleVal);
         move= true;
        }
-
  }
-
-
-   //String input = "1,0,2,0,3,0,4,0,5,0,6,30,7,100,8,100,";
 
 
   if(move){
